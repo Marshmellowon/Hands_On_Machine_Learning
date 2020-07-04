f661d449cf8bfeb0118f16088d888cbfdeb43206
Marshmellowon <leo503801@gmail.com>
2020-07-04 16:03:17 +0900

    push:다차원 배열의 계산

---
+++ Neural_Network/matrix.py
@@ -0,0 +1,12 @@
+# 다차원 배열의 계산
+import numpy as np
+
+A = np.array([1, 2, 3, 4])
+
+print("A의 차원: ", np.ndim(A))
+
+print("배열의 형상: ", A.shape)
+
+B = np.array([[1, 2], [3, 4], [5, 6]])
+
+print("B의 차원: ", np.ndim(B))
